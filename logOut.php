5e8f7811781867247908657982de99222bf33b65
dianaDanial <diana9a11@gmail.com>
2019-03-09 13:24:22 +0800

    Create logOut.php

---
+++ logOut.php
@@ -0,0 +1,13 @@
+<?php
+
+	//start PHP session
+	session_start();
+
+	//empty PHP session
+	$_SESSION = array();
+
+	//show message to user about logout
+	$_SESSION['passThruMessage']="You have logged out.";
+	header("Location: index.php");
+
+?>
