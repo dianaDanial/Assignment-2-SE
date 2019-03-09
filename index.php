4e8c20aaecad7a4bd3228dc915308142fee2bccf
dianaDanial <diana9a11@gmail.com>
2019-03-09 16:53:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -33,19 +33,19 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 
   <div class="w3-bar w3-red w3-card w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Programmes</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Sign Up</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contact Us</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Login</a>
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+    <a href="programmes.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Programmes</a>
+    <a href="signUpSASadmin.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Sign Up</a>
+    <a href="contactUs.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contact Us</a>
+    <a href="login.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Login</a>
   </div>
 
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Programmes</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Sign Up</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Contact Us</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Login</a>
+    <a href="programmes.php" class="w3-bar-item w3-button w3-padding-large">Programmes</a>
+    <a href="signUpSASadmin.php" class="w3-bar-item w3-button w3-padding-large">Sign Up</a>
+    <a href="contactUs.php" class="w3-bar-item w3-button w3-padding-large">Contact Us</a>
+    <a href="login.php" class="w3-bar-item w3-button w3-padding-large">Login</a>
   </div>
 </div>
 
