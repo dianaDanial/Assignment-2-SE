11335500f0dd1dcfa020108aff2e7bb9ee741c42
dianaDanial <diana9a11@gmail.com>
2019-03-09 16:34:44 +0800

    Create registerUni.php

---
+++ registerUni.php
@@ -0,0 +1,81 @@
+<?php
+include ('connection.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<title>Register University</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/x-icon" href="logo.ico">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <style>
+  body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+  .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+  .fa-anchor,.fa-coffee {font-size:200px}
+  </style>
+
+  <!-- Navbar -->
+
+  <div class="w3-top">
+    <body class="bg-info" >
+  <nav class="navbar navbar-expand-md bg-light navbar-light">
+      <img src="logo.png" alt="Logo" style="width:50px; height:85px">
+      <a class="navbar-brand" href="index.html"style="font-family: "Times New Roman", Times, serif;">HighEd</a>
+    <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+      <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+      <a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Home</a>
+      <a href="programmes.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Programmes</a>
+      <a href="maintainQualifications.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Maintain Qualifications</a>
+      <a href="registerUni.php" class="w3-bar-item w3-button w3-padding-large w3-white">Register University</a>
+      <a href="logOut.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Log Out</a>
+    </div>
+
+    <!-- Navbar on small screens -->
+    <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+      <a href="index.php" class="w3-bar-item w3-button w3-padding-large">Home</a>
+      <a href="programmes.php" class="w3-bar-item w3-button w3-padding-large">Programmes</a>
+      <a href="maintainQualifications.php" class="w3-bar-item w3-button w3-padding-large">Maintain Qualifications</a>
+      <a href="logOut.php" class="w3-bar-item w3-button w3-padding-large">Log Out</a>
+    </div>
+  </div>
+  <div class="w3-container w3-light-grey w3-center" style="padding:80px 16px; "
+    <div class="card"  "center" style="width:auto;" >
+      <br>
+      <br>
+      <div class="form-group">
+        <img class="card-img-top mx-auto d-block" src="register.png" alt=" image icon of register" style="width:20%;" >
+        <br>
+        <h2 style="color:#000080 ;text-decoration: underline;">Record University</h2>
+        <form action="registerUni.php" method="post">
+          <div class="container mt-3">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text">University</span>
+            </div>
+            <input type="text" class="form-control" placeholder="Enter University Name" id="universityName" name="universityName" value="<?php echo $universityName;?>">
+          </div>
+        </div>
+        <button type="submit" class="btn btn-info btn-md btn-block" name="register_Uni">Register University</button>
+      </form>
+    </div>
+  </div>
+</body>
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-64 w3-center w3-opacity">
+    <div class="w3-xlarge w3-padding-32">
+      <i class="fa fa-facebook-official w3-hover-opacity"></i>
+      <i class="fa fa-instagram w3-hover-opacity"></i>
+      <i class="fa fa-snapchat w3-hover-opacity"></i>
+      <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+      <i class="fa fa-twitter w3-hover-opacity"></i>
+      <i class="fa fa-linkedin w3-hover-opacity"></i>
+   </div>
+   <p style="color: white; font-size: 20px; ">Copyright by Sharifah & Khadijah</p>
+  </footer>
