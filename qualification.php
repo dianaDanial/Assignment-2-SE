c020ec2ba35c5ea6e0aa81a15ae4bd63891d88b7
khadijahroslan <kha89@live.com>
2019-03-14 21:09:09 +0800

    update working insert

--- qualification.php
+++ qualification.php
@@ -1,33 +1,87 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'highed');
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully<br>";
+//if(isset($_POST['set'])){
+//if (!(empty($_POST["name"]) && empty($_POST["minscore"]) && empty($_POST["maxscore"]) && empty($_POST["desc"]) && empty($_POST["noofsubject"]))) {
+//if( isset( $_POST['name'], $_POST['minscore'], $_POST['maxscore'], $_POST['desc'], $_POST['noofsubject']) ) {
+/*
+    $host   =   'localhost';
+    $uname  =   'root'; 
+    $pwd    =   ''; 
+    $db     =   'highed';
 
-$name = $_POST["name"];
-$minscore = $_POST["minscore"];
-$maxscore = $_POST["maxscore"];
-$desc = $_POST["desc"];
-$noofsubject = $_POST["noofsubject"];
-if (isset($_POST["set"])) {
-    if (!(empty($_POST["name"]) && empty($_POST["minscore"]) && empty($_POST["maxscore"]) && empty($_POST["desc"]) && empty($_POST["noofsubject"]))) {
-        $sql = "INSERT INTO qualification (name, minscore, maxscore, desc, noofsubject) VALUES ('$name', '$minscore', '$maxscore', '$desc', '$noofsubject')";
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
+    $conn   =   new mysqli( $host, $uname, $pwd, $db );
 
-        $conn->close();
-        
+    if ( !$conn ) {
+        die("Connection failed: " . mysqli_connect_error() );
     }
-    
+
+/*
+    $sql="insert into qualification ( name, minscore, maxscore, description, noofsubject ) values (  ?,?,?,?,? )";
+    $stmt=$conn->prepare( $sql );
+
+    if( $stmt ){
+
+        $name=$_POST['name'];
+        $minscore=$_POST['minscore'];
+        $maxscore=$_POST['maxscore'];
+        $desc=$_POST['desc'];
+        $noofsubject=$_POST['noofsubject'];
+        //$avail=$_POST['availabilty'];
+
+          //  use i for integers
+          //  use s for strings
+        
+        $stmt->bind_params( 'siisi', $name,$minscore,$maxscore,$desc,$noofsubject );
+        $result=$stmt? 'Success!' : 'Fail!';
+
+        $stmt->close();
+        $conn->close();
+
+    } else {
+        echo 'Error creating statement';
+    }
+} else {
+    echo 'One or more required POST variables are not set';
+}*/
+//}
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "highed";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//$conn = mysqli_connect('localhost', 'root', '', 'highed');
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+$name = $_POST['name'];
+ $minscore = $_POST['minscore'];
+ $maxscore = $_POST['maxscore'];
+ $desc = $_POST['desc'];
+ $noofsubject = $_POST['noofsubject'];
+ 
+ //echo $name; echo $minscore; echo $maxscore; echo $desc; echo $noofsubject;
+ if(isset($_POST['set'])){
+     if (!(empty($_POST["name"]) && empty($_POST["minscore"]) && empty($_POST["maxscore"]) && empty($_POST["desc"]) && empty($_POST["noofsubject"]))) {
+ 
+ $sql = "INSERT INTO qualification (name,minscore,maxscore,description,noofsubject) VALUES ('$name','$minscore','$maxscore','$desc','$noofsubject')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+ }
+ }
+mysqli_close($conn);
+
+
 
 ?>
-
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -41,12 +95,12 @@ and open the template in the editor.
     </head>
     <body>
         <h1>Set Up Qualifications</h1><br>
-        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <form action="qualification.php" method="post">
             Qualification Name: <input type="text" name="name"><br>
             Minimum Score: <input type="number" name="minscore">
             Maximum Score: <input type="number" name="maxscore"><br>
-            <input type="radio" name="desc" value="total" > Total
-            <input type="radio" name="desc" value="average" > Average,
+            <input type="radio" name="desc" value="Total" > Total
+            <input type="radio" name="desc" value="Average" > Average,
             No of Subject: <input type="number" name="noofsubject"><br>
             <input type="submit" name="addsubject" value="Add Subjects and Scores"><br>
             <input type="submit" name="set" value="Set Qualification">
