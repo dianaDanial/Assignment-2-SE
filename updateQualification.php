8a5d1bfaefaa088b474c8b6cabe822c44d1a533b
dianaDanial <diana9a11@gmail.com>
2019-04-02 14:39:26 +0800

    Update updateQualification.php

--- updateQualification.php
+++ updateQualification.php
@@ -55,27 +55,26 @@ if (isset($_POST['update_qualification'])){
 
   <div class="w3-top">
     <body class="bg-info" >
-  <nav class="navbar navbar-expand-md bg-light navbar-light">
-      <img src="logo.png" alt="Logo" style="width:50px; height:85px">
-      <a class="navbar-brand" href="index.html"style="font-family: "Times New Roman", Times, serif;">HighEd</a>
-    <div class="w3-bar w3-red w3-card w3-left-align w3-large">
-      <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-      <a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Home</a>
-      <a href="maintainQualifications.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Maintain Qualifications</a>
-      <a href="registerUni.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Register University</a>
-      <a href="logOut.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Log Out</a>
-    </div>
+      <div class="w3-top">
 
-    <!-- Navbar on small screens -->
-    <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-      <a href="#" class="w3-bar-item w3-button w3-padding-large">Home</a>
-      <a href="#" class="w3-bar-item w3-button w3-padding-large">Maintain Qualifications</a>
-      <a href="#" class="w3-bar-item w3-button w3-padding-large">Record University</a>
-      <a href="#" class="w3-bar-item w3-button w3-padding-large">Log Out</a>
+          <div class="w3-bar w3-black w3-card">
+
+              <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+              <a href="index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+              <a href="maintainQualifications.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Maintain Qualification</a>
+              <a href="registerUni.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Register University</a>
+							<a href="viewAdmin.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">View University Admin</a>
+              <a href="logOut.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Logout</a>
+              <div class="w3-dropdown-hover w3-hide-small">
+
+              </div>
+
+								<h5 class="w3-padding-large w3-hide-small w3-right">Hi ,<?php echo $_SESSION['username']; ?> </h5>
+          </div>
+      </div>
     </div>
-  </div>
-  <div class="w3-container w3-light-grey w3-center" style="padding:80px 16px; "
-  <div class="container">
+  <div class="w3-container w3-white w3-center" style="padding:80px 16px; ">
+
   <div class="w3-content">
     <div class= "w3-center"style="width:auto;">
       <br>
@@ -118,15 +117,16 @@ if (isset($_POST['update_qualification'])){
       <textarea class="form-control"  placeholder="Enter the description" id="resultCalcDescription"name = "resultCalcDescription" value="<?php echo $resultCalcDescription;?>"></textarea>
       </div>
       <br>
-      <button type="submit" class="btn btn-success btn-lg btn-block" name="update_qualification">Save</button>
+      <button type="submit" class="btn btn-info btn-lg btn-block" name="update_qualification">Save</button>
     </form>
   </div>
 </div>
 </div>
 </body>
   <!-- Footer -->
-  <footer class="w3-container w3-padding-64 w3-center w3-opacity">
-    <div class="w3-xlarge w3-padding-32">
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-32 w3-center w3-black">
+    <div class="w3-large w3-padding-20">
       <i class="fa fa-facebook-official w3-hover-opacity"></i>
       <i class="fa fa-instagram w3-hover-opacity"></i>
       <i class="fa fa-snapchat w3-hover-opacity"></i>
@@ -136,15 +136,4 @@ if (isset($_POST['update_qualification'])){
    </div>
    <p style="color: white; font-size: 20px; ">Copyright by Sharifah & Khadijah</p>
   </footer>
-  <script>
-  // Used to toggle the menu on small screens when clicking on the menu button
-  function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-  </script>
 </html>
