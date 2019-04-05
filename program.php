0c55ce018170d5eca291bb53062ff4a28b5245ef
dianaDanial <diana9a11@gmail.com>
2019-04-05 17:00:40 +0800

    Update program.php

--- program.php
+++ program.php
@@ -23,28 +23,24 @@ include ('connection.php');
 
   <!-- Navbar -->
 
-  <div class="w3-top">
-    <body class="bg-info" >
-  <nav class="navbar navbar-expand-md bg-light navbar-light">
-      <img src="logo.png" alt="Logo" style="width:50px; height:85px">
-      <a class="navbar-brand" href="index.html"style="font-family: "Times New Roman", Times, serif;">HighEd</a>
-    <div class="w3-bar w3-red w3-card w3-left-align w3-large">
-      <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-      <a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Home</a>
-      <a href="home2.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Maintain University Programme</a>
-      <a href="review.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Review Application</a>
-      <a href="logOut.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Log Out</a>
-    </div>
 
-    <!-- Navbar on small screens -->
-    <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-      <a href="index.php" class="w3-bar-item w3-button w3-padding-large">Home</a>
-      <a href="home2.php" class="w3-bar-item w3-button w3-padding-large">Maintain University Programme</a>
-      <a href="review.php" class="w3-bar-item w3-button w3-padding-large">Review Application</a>
-      <a href="logOut.php" class="w3-bar-item w3-button w3-padding-large">Log Out</a>
+    <body class="bg-info" >
+      <div class="w3-top">
+        <div class="w3-bar w3-black w3-card">
+
+          <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+          <a href="index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+          <a href="home2.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Manage Programme</a>
+          <a href="reviewProgramme.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Review Applications</a>
+          <a href="logOut.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Logout</a>
+          <div class="w3-dropdown-hover w3-hide-small">
+
+          </div>
+
+            <h5 class="w3-padding-large w3-hide-small w3-right">Hi ,<?php echo $_SESSION['username']; ?> </h5>
+      </div>
     </div>
-  </div>
-  <div class="w3-container w3-light-grey w3-center" style="padding:80px 16px; "
+  <div class="w3-container w3-white w3-center" style="padding:80px 16px; ">
   <div class="w3-content">
     <div class= "w3-center"style="width:auto;">
       <br>
@@ -77,29 +73,17 @@ include ('connection.php');
   </div>
 </div>
 </body>
-  <!-- Footer -->
-  <footer class="w3-container w3-padding-64 w3-center w3-opacity">
-    <div class="w3-xlarge w3-padding-32">
-      <i class="fa fa-facebook-official w3-hover-opacity"></i>
-      <i class="fa fa-instagram w3-hover-opacity"></i>
-      <i class="fa fa-snapchat w3-hover-opacity"></i>
-      <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-      <i class="fa fa-twitter w3-hover-opacity"></i>
-      <i class="fa fa-linkedin w3-hover-opacity"></i>
-   </div>
-   <p style="color: white; font-size: 20px; ">Copyright by Sharifah & Khadijah</p>
-  </footer>
+<!-- Footer -->
+<footer class="w3-container w3-padding-32 w3-center w3-black">
+  <div class="w3-large w3-padding-20">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+ </div>
+ <p style="color: white; font-size: 20px; ">Copyright by Sharifah & Khadijah</p>
+</footer>
 
-  <script>
-  // Used to toggle the menu on small screens when clicking on the menu button
-  function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-</script>
-</body>
 </html>
