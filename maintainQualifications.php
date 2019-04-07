bd1e25bcf273c5802500c36c02f1f09b440ad51f
dianaDanial <diana9a11@gmail.com>
2019-04-07 21:41:55 +0800

    Update maintainQualifications.php

--- maintainQualifications.php
+++ maintainQualifications.php
@@ -147,16 +147,5 @@ if ($result_select_qualification = mysqli_query($db, $sql_select_qualification))
    <p style="color: white; font-size: 20px; ">Copyright by Sharifah & Khadijah</p>
   </footer>
 
-  <script>
-  // Used to toggle the menu on small screens when clicking on the menu button
-  function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-</script>
 </body>
 </html>
