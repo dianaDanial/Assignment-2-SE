f91ae5f33a0293e74c35e0b702745db9cbcbdaea
dianaDanial <diana9a11@gmail.com>
2019-04-07 21:41:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -79,30 +79,19 @@
     </div>
   </div>
 </body>
-  <!-- Footer -->
-  <footer class="w3-container w3-padding-64 w3-center w3-opacity">
-    <div class="w3-xlarge w3-padding-32">
-      <i class="fa fa-facebook-official w3-hover-opacity"></i>
-      <i class="fa fa-instagram w3-hover-opacity"></i>
-      <i class="fa fa-snapchat w3-hover-opacity"></i>
-      <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-      <i class="fa fa-twitter w3-hover-opacity"></i>
-      <i class="fa fa-linkedin w3-hover-opacity"></i>
-   </div>
-   <p style="color: white; font-size: 20px; ">Copyright by Sharifah & Khadijah</p>
-  </footer>
+<!-- Footer -->
+<footer class="w3-container w3-padding-32 w3-center w3-black">
+  <div class="w3-large w3-padding-20">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+ </div>
+ <p style="color: white; font-size: 20px; ">Copyright by Sharifah & Khadijah</p>
+</footer>
 
-  <script>
-  // Used to toggle the menu on small screens when clicking on the menu button
-  function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-  </script>
 
   </body>
   </html>
