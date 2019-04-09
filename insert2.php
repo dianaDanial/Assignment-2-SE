24f6460f57e3000703ab7780646f343987c0059c
dianaDanial <diana9a11@gmail.com>
2019-04-10 07:50:06 +0800

    Create insert2.php

---
+++ insert2.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_SESSION["username"]))
+{
+ $connect = new PDO("mysql:host=localhost;dbname=highEd", "root", "");
+ for($count = 0; $count < count($_POST["username"]); $count++)
+ {
+  $query = "INSERT INTO result
+  (applicant,subjectName, grade, score)
+  VALUES ('$_SESSION["username"]',:subjectName, :grade, :score)
+  ";
+  $statement = $connect->prepare($query);
+  $statement->execute(
+   array(
+    ':subjectName'  => $_POST["subjectName"][$count],
+    ':grade' => $_POST["grade"][$count],
+    ':score'  => $_POST["score"][$count],
+   )
+  );
+ }
+ $result = $statement->fetchAll();
+ if(isset($result))
+ {
+  echo 'ok';
+ }
+}
+?>
