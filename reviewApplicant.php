7ae5e3ea0e1315b6dd5190581d45703bd90934ba
dianaDanial <diana9a11@gmail.com>
2019-04-10 04:36:29 +0800

    Update reviewApplicant.php

--- reviewApplicant.php
+++ reviewApplicant.php
@@ -52,7 +52,7 @@ if (isset($_POST['submit_page'])){
 
 					<a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
 					<a href="index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
-					<a href="home2.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Manage Programme</a>
+					<a href="manageProgramme.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Manage Programme</a>
 					<a href="reviewProgramme.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Review Applications</a>
 					<a href="logOut.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"  style="float:right">Logout</a>
 					<div class="w3-dropdown-hover w3-hide-small">
